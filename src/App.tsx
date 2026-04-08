35d5a5d5561017d966043e631e3814f620bd2d49
esuder12-gif <esuder12@gmail.com>
2026-04-08 15:58:21 +0300

    Import useEffect in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Toaster } from 'sonner';
 import { Navbar } from './components/layout/Navbar';
 import { Hero } from './components/home/Hero';
